8c811cc63a73df35788d7e6c93605dd0d794ca19
unknown <enderaoelyther@gmail.com>
2023-03-15 17:26:16 +0800

    competitive with GZCTF

--- Web_Challs/Php_Mysql_challs/start.sh
+++ Web_Challs/Php_Mysql_challs/start.sh
@@ -1,4 +1,19 @@
-docker build -t safe_house .
-docker-compose up --build -d
-docker cp ./agentxer.sql safe_house_sql:/
-docker exec safe_house_sql /bin/sh -c 'mysql -u root -pscam@123 agentxer </agentxer.sql'
+#!/bin/sh
+
+sed -i 's/flag{test_flag_string_babecafe}/'"$GZCTF_FLAG"'/' /root/database.sql
+
+RUN /etc/init.d/mysql start &&\
+    mysql -e "grant all privileges on *.* to 'root'@'%' identified by 'toor';"&&\
+    mysql -e "grant all privileges on *.* to 'root'@'localhost' identified by 'toor';"&&\
+    mysql -u root -ptoor -e "show databases;" &&\
+    mysql -u root -ptoor </root/database.sql &&\
+	mysql -u root -ptoor -e "show databases;"
+
+RUN sed -Ei 's/^(bind-address|log)/#&/' /etc/mysql/my.cnf \
+	&& echo 'skip-host-cache\nskip-name-resolve' | awk '{ print } $1 == "[mysqld]" && c == 0 { c = 1; system("cat") }' /etc/mysql/my.cnf > /tmp/my.cnf \
+	&& mv /tmp/my.cnf /etc/mysql/my.cnf
+
+# Apache gets grumpy about PID files pre-existing
+rm -f /usr/local/apache2/logs/httpd.pid
+service mysql start
+apache2ctl start
